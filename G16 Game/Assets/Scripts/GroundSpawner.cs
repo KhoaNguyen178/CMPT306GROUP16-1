e8c306cf93e65da841bfd63adbe60e9a32d3e834
Aiden Trinh <69366889+Silves1@users.noreply.github.com>
2022-11-13 14:45:39 -0600

    Update ground

--- G16 Game/Assets/Scripts/GroundSpawner.cs
+++ G16 Game/Assets/Scripts/GroundSpawner.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class GroundSpawner : MonoBehaviour
 {
-    public GameObject Ground1, Ground2, Ground3;
+    //public GameObject Ground1, Ground2, Ground3;
+    public GameObject[] ground;
+  
     bool hasGround = true;
     int randomNum = 1;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -20,32 +24,47 @@ public class GroundSpawner : MonoBehaviour
         {
             SpawnGround();
             hasGround = true;
+
         }
     }
 
+
     public void SpawnGround()
     {
-        //int randomNum = Random.Range(1, 4);
+        //int randomNum = Random.Range(1, 3);
+
+        //int idx = Random.Range(0, ground.Length);
+        int randomNum = Random.Range(0, ground.Length);
         
-        if (randomNum == 1)
+        if (randomNum == 0)
         {
-            Instantiate(Ground1, new Vector3(transform.position.x + 3, -4.574871f, 0), Quaternion.identity);
+            Instantiate(ground[0], new Vector3(transform.position.x + 14, -4.5498f, 0), Quaternion.identity);
         }
 
-        //if (randomNum == 2)
-        //{
-        //    Instantiate(Ground1, new Vector3(transform.position.x + 3, -2f, 0), Quaternion.identity);
-        //}
+        if (randomNum == 1)
+        {
+            Instantiate(ground[1], new Vector3(transform.position.x + 8, -2.52f, 0), Quaternion.identity);
+        }
 
-        //if (randomNum == 3)
-        //{
-        //    Instantiate(Ground1, new Vector3(transform.position.x + 3, -1f, 0), Quaternion.identity);
-        //}
+        if (randomNum == 2)
+        {
+            Instantiate(ground[2], new Vector3(transform.position.x + 4, -2.58f, 0), Quaternion.identity);
+        }
+
+        if (randomNum == 3)
+        {
+            Instantiate(ground[3], new Vector3(transform.position.x + 5, -2.56f, 0), Quaternion.identity);
+        }
+
+        if (randomNum == 4)
+        {
+            Instantiate(ground[4], new Vector3(transform.position.x + 9, -2.92f, 0), Quaternion.identity); //11
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))
+        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("MoveGround"))
         {
             hasGround = true;
         }
@@ -53,7 +72,7 @@ public class GroundSpawner : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))
+        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("MoveGround"))
         {
             hasGround = false;
         }
