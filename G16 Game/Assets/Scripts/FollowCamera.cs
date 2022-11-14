cae1321cac43960ccced0e42facc73f00f1c1105
Thao Bui (aka Mita) <71835356+mita1912@users.noreply.github.com>
2022-11-14 17:58:22 -0600

    A* Pathing for flying and on ground enemies.
    - A* Pathing for parallax background.
    - Add sounds effects.

--- G16 Game/Assets/Scripts/FollowCamera.cs
+++ G16 Game/Assets/Scripts/FollowCamera.cs
@@ -20,5 +20,6 @@ public class FollowCamera : MonoBehaviour
     {
         GridGraph.astarData.gridGraph.center = new Vector3(player.transform.position.x + offset.x, GridGraph.astarData.gridGraph.center.y,
             GridGraph.astarData.gridGraph.center.z);
+        AstarPath.active.Scan();
     }
 }
