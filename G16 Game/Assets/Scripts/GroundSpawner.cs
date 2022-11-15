ea3919aff20cd5ed4bbbe463dd3b03e86764a148
Aiden Trinh <69366889+Silves1@users.noreply.github.com>
2022-11-15 01:02:08 -0600

    Update

--- G16 Game/Assets/Scripts/GroundSpawner.cs
+++ G16 Game/Assets/Scripts/GroundSpawner.cs
@@ -37,69 +37,82 @@ public class GroundSpawner : MonoBehaviour
         if (randomNum == 0)
         {
             Instantiate(ground[0], new Vector3(transform.position.x + 14, -4.5498f, 0), Quaternion.identity);
-            
+            isSpawned = true;
         }
 
         if (randomNum == 1)
         {
             Instantiate(ground[1], new Vector3(transform.position.x + 8, -2.52f, 0), Quaternion.identity);
-       
+            isSpawned = true;
         }
 
         if (randomNum == 2)
         {
             Instantiate(ground[2], new Vector3(transform.position.x + 4, -2.58f, 0), Quaternion.identity);
-          
+            isSpawned = true;
         }
 
         if (randomNum == 3)
         {
             Instantiate(ground[3], new Vector3(transform.position.x + 5, -2.56f, 0), Quaternion.identity);
-         
+            isSpawned = true;
         }
 
         if (randomNum == 4)
         {
             Instantiate(ground[4], new Vector3(transform.position.x + 9, -2.92f, 0), Quaternion.identity); //11
-      
+            isSpawned = true;
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+
+        float horizontalMove = Input.GetAxis("Horizontal");
+
         if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("MoveGround"))
         {
             hasGround = true;
-         
+            Debug.Log("has ground");
         }
+
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         float horizontalMove = Input.GetAxis("Horizontal");
+        
+        if (collision.gameObject.CompareTag("Ground") && horizontalMove < 0)
+        {
+            hasGround = true;
 
-        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("MoveGround"))
-        {   if(horizontalMove < 0)
-            {
-                hasGround = true;
-            }
-          
-            //if(collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("MoveGround") && horizontalMove >= 0)
-            //{
-            //    hasGround = true;
-            //}
-            
-            else if (horizontalMove <= 0 && horizontalMove >= 0)
-            {
-              hasGround = true;
-            }
-         
-        
+            Debug.Log("left");
         }
-        
-           if(horizontalMove >= 0)
+
+        //if (collision.gameObject.CompareTag("Ground") && horizontalMove > 0)
+        //{
+        //    hasGround = true;
+
+        //    Debug.Log("right");
+        //}
+
+        if (horizontalMove > 0 && collision.gameObject.CompareTag("Ground"))
+        {
+            hasGround = true;
+          
+            Debug.Log("right");
+        }
+
+
+
+        if (horizontalMove > 0)
         {
             hasGround = false;
+            Debug.Log("spawn");
         }
+      
+
     }
+
+
 }
