551fb9392b36ff7c1a6db912d042f56dc6eefa74
Ryan Lam <lamryan0815@gmail.com>
2022-11-23 19:56:58 -0600

    reset progresskills and adjust some colors for buttons

--- G16 Game/Assets/Scripts/CloseMenu.cs
+++ G16 Game/Assets/Scripts/CloseMenu.cs
@@ -2,23 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UpgradeController : MonoBehaviour
+public class CloseMenu : MonoBehaviour
 {
+    public GameObject upgradeMenu;
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
-
     public void onCloseClick()
     {
-
+        upgradeMenu.SetActive(false);
+        Time.timeScale = 1;
+        GameManager.instance.resetProgress();
     }
 }
-
