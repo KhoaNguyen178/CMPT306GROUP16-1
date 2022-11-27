890163537761c278d0c30aecc9de35e7360120f1
Aiden Trinh <69366889+Silves1@users.noreply.github.com>
2022-11-27 13:48:01 -0600

    Block enemy chasing player
    Done

--- G16 Game/Assets/Scripts/EnemyGroundMoving.cs
+++ G16 Game/Assets/Scripts/EnemyGroundMoving.cs
@@ -16,45 +16,59 @@ public class EnemyGroundMoving : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if (Vector2.Distance(transform.position, playerTransform.position) < chaseDistance)
+        {
+            isChasing = true;
+        }
+
+        if (Vector2.Distance(transform.position, playerTransform.position) > chaseDistance)
+        {
+            isChasing = false;
+            Debug.Log("NOT CHASING");
+        }
+
         if (isChasing)
         {
-            if(transform.position.x > playerTransform.position.x)
+            if (transform.position.x > playerTransform.position.x)
             {
                 transform.localScale = new Vector3(1, 1, 1);
                 transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+                patrolDestination = 1;
+
             }
-            if(transform.position.x < playerTransform.position.x)
+            if (transform.position.x < playerTransform.position.x)
             {
                 transform.localScale = new Vector3(-1, 1, 1);
                 transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+                patrolDestination = 0;
             }
         }
-        else
-        {   
-            if(Vector2.Distance(transform.position, playerTransform.position) < chaseDistance)
+
+        if (!isChasing)
+        {
+            if (patrolDestination == 0)
             {
-                isChasing = true;
+                transform.position = Vector2.MoveTowards(transform.position, checkpoint[0].position, moveSpeed * Time.deltaTime);
+                if (Vector2.Distance(transform.position, checkpoint[0].position) < .2f)
+                {
+                    transform.localScale = new Vector3(1, 1, 1);
+                    patrolDestination = 1;
+                }
             }
 
-            if(patrolDestination == 0)
-                    {
-                        transform.position = Vector2.MoveTowards(transform.position, checkpoint[0].position, moveSpeed * Time.deltaTime);
-                        if(Vector2.Distance(transform.position, checkpoint[0].position) < .2f)
-                        {
-                            transform.localScale = new Vector3(1, 1, 1);
-                            patrolDestination = 1;
-                        }
-                    }
             if (patrolDestination == 1)
-                    {
-                        transform.position = Vector2.MoveTowards(transform.position, checkpoint[1].position, moveSpeed * Time.deltaTime);
-                        if (Vector2.Distance(transform.position, checkpoint[1].position) < .2f)
-                        {
-                            transform.localScale = new Vector3(-1, 1, 1);
-                            patrolDestination = 0;
-                        }
-                    }
+            {
+                transform.position = Vector2.MoveTowards(transform.position, checkpoint[1].position, moveSpeed * Time.deltaTime);
+                if (Vector2.Distance(transform.position, checkpoint[1].position) < .2f)
+                {
+                    transform.localScale = new Vector3(-1, 1, 1);
+                    patrolDestination = 0;
+                }
+            }
         }
-        
+       
+
+
     }
 }
