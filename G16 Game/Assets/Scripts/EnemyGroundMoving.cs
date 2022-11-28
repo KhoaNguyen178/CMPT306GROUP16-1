d8a9e3d1479b6fe6f8e9e94419781e2b58d1a380
Aiden Trinh <69366889+Silves1@users.noreply.github.com>
2022-11-28 14:12:16 -0600

    Update

--- G16 Game/Assets/Scripts/EnemyGroundMoving.cs
+++ G16 Game/Assets/Scripts/EnemyGroundMoving.cs
@@ -59,22 +59,22 @@ public class EnemyGroundMoving : MonoBehaviour
 
        
   
-            if (groundInfor.collider == false)
+        if (groundInfor.collider == false)
+        {
+            if (!isChasing)
             {
-                if (!isChasing)
+                if (movingRight == true)
                 {
-                    if (movingRight == true)
-                    {
-                        transform.eulerAngles = new Vector3(0, -180, 0);
-                        movingRight = false;
-                    }
-                    else
-                    {
-                        transform.eulerAngles = new Vector3(0, 0, 0);
-                        movingRight = true;
-                    }
+                    transform.eulerAngles = new Vector3(0, -180, 0);
+                    movingRight = false;
                 }
-            
+                else
+                {
+                    transform.eulerAngles = new Vector3(0, 0, 0);
+                    movingRight = true;
+                }
             }
+            
+        }
     }
 }
