e769fbaddf240a361eaf9001d152618abf0ce839
Thao Bui (aka Mita) <71835356+mita1912@users.noreply.github.com>
2022-11-27 18:34:15 -0600

    AI Patrolling + Player Following for Ground Enemies (valid for each ground).

--- G16 Game/Assets/Scripts/AIPatrol.cs
+++ G16 Game/Assets/Scripts/AIPatrol.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class AIPatrol : MonoBehaviour
 {
+<<<<<<< Updated upstream
     public Rigidbody2D rb;
     public float walkSpeed;
     private float dirX;
@@ -76,5 +77,52 @@ public class AIPatrol : MonoBehaviour
             localScale.x *= -1;
         }
         transform.localScale = localScale;
+=======
+    public float walkSpeed;
+
+    [HideInInspector] public bool mustPatrol;
+
+    public Rigidbody2D rb;
+    public Transform groundCheckPos;
+    public LayerMask groundlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mustPatrol = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mustPatrol)
+        {
+            Patrol();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (mustPatrol)
+        {
+            mustTurn = !Physics2D.OverlapCircle(groundCheckPos.position, 0.1f, groundLayer);
+        }
+    }
+
+
+    void Patrol()
+    {
+        if (mustTurn)
+        {
+            Flip();
+        }
+        rb.velocity = new Vector2(walkSpeed * Time.fixedDeltaTime, rb.velocity.y);
+    }
+
+    void Flip()
+    {
+        transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+        walkSpeed *= -1;
+>>>>>>> Stashed changes
     }
 }
