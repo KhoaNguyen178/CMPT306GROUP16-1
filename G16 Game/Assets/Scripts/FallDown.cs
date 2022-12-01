0430ada3ab66917c43c7c86f7c51607c671e0902
Aiden Trinh <69366889+Silves1@users.noreply.github.com>
2022-11-30 23:24:30 -0600

    Update enemy health rate

--- G16 Game/Assets/Scripts/FallDown.cs
+++ G16 Game/Assets/Scripts/FallDown.cs
@@ -5,23 +5,45 @@ using UnityEngine;
 public class FallDown : MonoBehaviour
 {
     Rigidbody2D rb;
-    // Start is called before the first frame update
+    Vector2 previousPlatform;
+    bool platforMovingBack;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        previousPlatform = transform.position;
+    }
+
+    void Update()
+    {
+        if (platforMovingBack)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, previousPlatform, 20f * Time.deltaTime);
+        }
+        if(transform.position.y == previousPlatform.y)
+        {
+            platforMovingBack = false;
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") && !platforMovingBack)
         {
             Invoke("DropPlatform", 0.5f);
-            Destroy(gameObject, 2f);
         }
     }
 
     void DropPlatform()
     {
         rb.isKinematic = false;
+        Invoke("GetPlatformBack", 1f);
+    }
+
+    void GetPlatformBack()
+    {
+        rb.velocity = Vector2.zero;
+        rb.isKinematic = true;
+        platforMovingBack = true;
     }
 }
